61976f0f6a15f891497f0d546c10f32b7f68e8bf
zjhfp <zjhfp@163.com>
2017-03-16 20:07:24 +0800

    finis

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,8 +1,7 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
+export class Hero {
+  id: number;
+  name: string;
+}
 
 
 /*
